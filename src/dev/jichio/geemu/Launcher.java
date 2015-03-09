4f53f1aa9bccad37831fc4b726bbb34f4b1c1863
Kovarnijpoloter <firiat69@gmail.com>
2015-03-09 19:34:08 +0300

    Изменили название игры

--- src/dev/jichio/geemu/Launcher.java
+++ src/dev/jichio/geemu/Launcher.java
@@ -4,7 +4,7 @@ package dev.jichio.geemu;
 public class Launcher {
 
     public static void main(String[] args){
-        Game game = new Game("DEMO", 1024, 768);
+        Game game = new Game("夢の庭[Yume no Niwa] — [Cад✿Снов]", 1024, 768);
         game.start();
     }
 
