3c09ca41ab66b36214b360f280ad81751d3cca54
Yukaio <Shinoda110@gmail.com>
2015-03-15 17:34:07 +0300

    Реализовали возможность задать базовый размер объекта, а так же его свойства (скорость). На основе возможности задать базовые свойства объекта переделали механизм перемещения объекта.

--- src/dev/jichio/geemu/entities/creatures/Player.java
+++ src/dev/jichio/geemu/entities/creatures/Player.java
@@ -10,24 +10,34 @@ public class Player extends Creature{
     private Game game;
 
     public Player(Game game,float x, float y) {
-        super(x, y);
+        super(x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
         this.game = game;
     }
 
     @Override
     public void tick() {
-        if(game.getKeyManager().up || game.getKeyManager().up1)
-            y -=3;
-        if(game.getKeyManager().down || game.getKeyManager().down1)
-            y +=3;
-        if(game.getKeyManager().left || game.getKeyManager().left1)
-            x -=3;
-        if(game.getKeyManager().right || game.getKeyManager().right1)
-            x +=3;
+        getInput();
+        move();
+    }
+
+    private void getInput(){
+        xMove = 0;
+        yMove = 0;
+
+        if (game.getKeyManager().up || game.getKeyManager().up1)
+            yMove = -speed;
+        if (game.getKeyManager().down || game.getKeyManager().down1)
+            yMove = speed;
+        if (game.getKeyManager().left || game.getKeyManager().left1)
+            xMove = -speed;
+        if (game.getKeyManager().right || game.getKeyManager().right1)
+            xMove = speed;
+
+
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player, (int) x, (int) y, null);
+        g.drawImage(Assets.player, (int) x, (int) y, width, height, null);
     }
 }
