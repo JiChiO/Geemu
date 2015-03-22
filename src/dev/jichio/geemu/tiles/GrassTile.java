76fdbed4578b5fc352041e35f7937fdeb1aa0435
Kovarnijpoloter <firiat69@gmail.com>
2015-03-22 13:07:17 +0300

    ...

--- src/dev/jichio/geemu/tiles/GrassTile.java
+++ src/dev/jichio/geemu/tiles/GrassTile.java
@@ -3,14 +3,9 @@ package dev.jichio.geemu.tiles;
 
 import dev.jichio.geemu.gfx.Assets;
 
-public class GrassTile extends Tile{
+public class GrassTile extends Tile {
     public GrassTile(int id) {
         super(Assets.grass, id);
-    }
-
-    @Override
-    public boolean isSolid(){
-        return true;
     }
 }
 
